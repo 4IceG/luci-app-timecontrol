c2fc0db25cfcf9fb99493de588dceaa9a312ea10
Rafał Wabik <4rafal@gmail.com>
2020-11-02 11:29:49 +0100

    Add files via upload

---
+++ luci-app-timecontrol/luasrc/controller/timecontrol.lua
@@ -0,0 +1,15 @@
+module("luci.controller.timecontrol", package.seeall)
+
+function index()
+    if not nixio.fs.access("/etc/config/timecontrol") then return end
+
+    entry({"admin", "services", "timecontrol"}, cbi("timecontrol"), translate("Internet Time Control"), 60).dependent = true
+    entry({"admin", "services", "timecontrol", "status"}, call("status")).leaf = true
+end
+
+function status()
+    local e = {}
+    e.status = luci.sys.call("iptables -L FORWARD | grep TIMECONTROL >/dev/null") == 0
+    luci.http.prepare_content("application/json")
+    luci.http.write_json(e)
+end
