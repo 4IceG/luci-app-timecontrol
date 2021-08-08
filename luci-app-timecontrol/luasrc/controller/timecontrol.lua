32f76b0bc0b991af7bcb5ae1fd5c7980261d1799
Rafał Wabik <4rafal@gmail.com>
2021-08-08 06:05:58 -0700

    Up to 1.1 | Fixed application visibility before logging in.

--- luci-app-timecontrol/luasrc/controller/timecontrol.lua
+++ luci-app-timecontrol/luasrc/controller/timecontrol.lua
@@ -1,15 +1,12 @@
-module("luci.controller.timecontrol", package.seeall)
-
+module("luci.controller.timecontrol",package.seeall)
 function index()
-    if not nixio.fs.access("/etc/config/timecontrol") then return end
-
-    entry({"admin", "services", "timecontrol"}, cbi("timecontrol"), translate("Internet Time Control"), 60).dependent = true
-    entry({"admin", "services", "timecontrol", "status"}, call("status")).leaf = true
+if not nixio.fs.access("/etc/config/timecontrol")then return end
+entry({"admin","services","timecontrol"},cbi("timecontrol"),translate("Internet Time Control"),60).acl_depends={ "luci-app-timecontrol" }
+entry({"admin","services","timecontrol","status"},call("status")).leaf=true
 end
-
 function status()
-    local e = {}
-    e.status = luci.sys.call("iptables -L FORWARD | grep TIMECONTROL >/dev/null") == 0
-    luci.http.prepare_content("application/json")
-    luci.http.write_json(e)
+local e={}
+e.status=luci.sys.call("iptables -L FORWARD | grep TIMECONTROL >/dev/null")==0
+luci.http.prepare_content("application/json")
+luci.http.write_json(e)
 end
